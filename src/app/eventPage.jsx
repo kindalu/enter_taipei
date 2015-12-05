06ca360b7ab0a1a94b61b39d1205ac1cac6d549d
Kinda Lu <kindalu@gmail.com>
2015-12-05 13:57:24 +0800

    set up react environment

---
+++ src/app/eventPage.jsx
@@ -0,0 +1,17 @@
+import React, { PropTypes, Component } from 'react';
+
+class EventPage extends Component {
+  constructor(props, context){
+    super(props, context); 
+  }
+
+  render(){
+    return ( 
+      <div>
+        Hello world!!
+      </div>
+    );
+  }
+}
+
+export default EventPage;
